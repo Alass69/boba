04e3018bb02dc3b86211b3ff362cb4700c88b664
Alass9000 <kbhinvestments526@gmail.com>
2025-01-28 16:25:47 +0100

    Initial commit

--- landing_blog/blog/admin.py
+++ landing_blog/blog/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from .models import Post
+from .forms import PostAdminForm
 
 class PostAdmin(admin.ModelAdmin):
+    form = PostAdminForm
     list_display = ('title', 'author', 'created_at')
     filter_horizontal = ('tags',)
 
--- landing_blog/blog/forms.py
+++ landing_blog/blog/forms.py
@@ -1,23 +1,19 @@
 from django import forms
-from .models import Comment
+from ckeditor.widgets import CKEditorWidget
+from .models import Post, Comment
 from django.contrib.auth.forms import UserCreationForm
 
-class CommentForm(forms.ModelForm):
-    parent_id = forms.IntegerField(widget=forms.HiddenInput(), required=False)
+class PostAdminForm(forms.ModelForm):
+    content = forms.CharField(widget=CKEditorWidget())
+    class Meta:
+        model = Post
+        fields = '__all__'
 
+class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
-        fields = ['content', 'parent_id']
-
-    def clean_parent_id(self):
-        parent_id = self.cleaned_data['parent_id']
-        if parent_id:
-            try:
-                Comment.objects.get(pk=parent_id)
-            except Comment.DoesNotExist:
-                raise forms.ValidationError("Invalid parent comment ID.")
-        return parent_id
+        fields = ['content']
 
 class RegistrationForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
-        fields = UserCreationForm.Meta.fields + ("email",)
+        fields = UserCreationForm.Meta.fields + ('email',)
